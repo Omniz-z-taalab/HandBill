fe45ac86d3f55f7fdc6d9c46c9654d8770c4cb49
omnia <omnia.z.talaab@gmail.com>
2022-12-01 01:25:49 +0200

    AboutUs

--- lib/src/ui/screens/aboutUsScreen.dart
+++ lib/src/ui/screens/aboutUsScreen.dart
@@ -42,7 +42,7 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
   ScrollController? _scrollController;
   late AboutUsBloc aboutUsBloc;
   CategoryBloc? _categoryBloc;
-   List<Banners>? banner;
+  List<Banners>? banner;
 
   @override
   void initState() {
@@ -53,56 +53,59 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
 
   @override
   void dispose() {
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AboutUsBloc, AboutUsState>(
-        listener: (context, state) {
-          if(state is AboutUsSuccessStates){
-            banner = state.items!.banners;
-            print(banner![0].id);
-            list = state.items!.videos;
+    return BlocConsumer<AboutUsBloc, AboutUsState>(listener: (context, state) {
+      if (state is AboutUsSuccessStates) {
+        banner = state.items!.banners;
+        print(banner![0].id);
+        list = state.items!.videos;
+        print(list![0].link);
+      }
+    }, builder: (context, state) {
+      return SingleChildScrollView(
+          child: SizedBox(
+              height: 900,
+              width: double.infinity,
+              child: Scaffold(
+                  appBar: AppBar(
+                      leading: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                      backgroundColor: Colors.white,
+                      title: Text(
+                        'AboutUs'.tr(),
+                        style: TextStyle(color: Colors.black, fontSize: 15),
+                      )),
+                  body: SingleChildScrollView(
+                      child: Column(children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                      child: Stack(
+                        children: [
+                          banner == null
+                              ? Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(30),
+                            ),
+                          )
+                              : CarouselSlider.builder(
 
-          }
-        },
-        builder: (context, state) {
-          return SingleChildScrollView(
-              child: SizedBox(
-                  height: 900,
-                  width: double.infinity,
-                  child: Scaffold(
-                      appBar: AppBar(
-                          leading: IconButton(
-                              icon: Icon(
-                                Icons.arrow_back_ios,
-                                color: Colors.black,
-                              ),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              }),
-                          backgroundColor: Colors.white,
-                          title: Text(
-                            'AboutUs'.tr(),
-                            style: TextStyle(color: Colors.black, fontSize: 15),
-                          )),
-                      body: SingleChildScrollView(
-                          child: Column(children: [
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Center(
-                          child:  Stack(
-                              children: [
-                                banner == null ?
-                                 Container()
-                                :CarouselSlider.builder(
-                                  itemCount: banner!.isNotEmpty
-                                      ? banner!.length
-                                      : 6,
-                                  itemBuilder:
-                                      (BuildContext context, int index, int idx) {
+                                  itemCount:
+                                      banner!.isNotEmpty ? banner!.length : 6,
+                                  itemBuilder: (BuildContext context, int index,
+                                      int idx) {
                                     if (banner!.isNotEmpty) {
                                       return SliderAboutWidget(
                                           model: banner![index]);
@@ -116,84 +119,120 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
                                       scrollDirection: Axis.horizontal,
                                       autoPlay: true,
                                       enableInfiniteScroll: true,
-                                      autoPlayInterval: Duration(milliseconds: 4000),
+                                      autoPlayInterval:
+                                          Duration(milliseconds: 4000),
                                       autoPlayCurve: Curves.easeOutSine,
+                                      clipBehavior: Clip.antiAlias,
                                       onPageChanged: (index, reason) {
                                         setState(() {
                                           _sliderPosition = index;
                                         });
                                       }),
                                 ),
-                              ],
-                            ),),
-                        SizedBox(
-                          height: 15,
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Center(
+                      child: Text(
+                        'AboutUSVideos '.tr(),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
                         ),
-
-                        Center(
-                          child: Text(
-                            'About US Videos '.tr(),
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                            ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      height: 500,
+                      child: ListView(children: [
+                        list == null
+                        ? Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(30),
                           ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        SizedBox(
-                          height: 300,
-                          child: ListView(children: [
-                            GridView.count(
-                                padding: EdgeInsets.fromLTRB(10, 0, 10, 16),
-                                childAspectRatio: 1 / 0.7,
-                                crossAxisCount: 2,
-                                crossAxisSpacing: 2,
-                                mainAxisSpacing: 2,
-                                shrinkWrap: true,
-                                primary: false,
-                                children: List.generate(4, (index) {
-                                  return GetVideos(list![index]);
-                                })),
-                            SizedBox(height: 80),
-                          ]),
                         )
-                        ])))
-              ));
-        });
+                        :GridView.count(
+                            padding: EdgeInsets.fromLTRB(10, 0, 10, 16),
+                            childAspectRatio: 1 / 0.7,
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 2,
+                            mainAxisSpacing: 2,
+                            shrinkWrap: true,
+                            primary: false,
+                            children: List.generate(list!.length, (index) {
+                              return VideoPlayerr(data: list![index],);
+                            })),
+                        SizedBox(height: 80),
+                      ]),
+                    )
+                  ])))));
+    });
   }
-  Widget GetVideos(Videos data ){
-    late VideoPlayerController controller = VideoPlayerController.network(
-        data!.link == null ?
-        "https://test.hand-bill.com/storage/uploads/about_us_app/video/1661279659_small_RZ75www1Ty.mp4"
-            :data!.link!)..initialize();
+}
 
+class VideoPlayerr extends StatefulWidget {
+  Videos data;
+
+  VideoPlayerr({required this.data}) : super();
+
+  @override
+  State<VideoPlayerr> createState() => _VideoPlayerrState();
+}
+
+class _VideoPlayerrState extends State<VideoPlayerr> {
+  late VideoPlayerController controller;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+     controller!.dispose();
+  }
+  loadVideoPlayer(){
+    controller = VideoPlayerController.network("${widget.data.link == null? 'https://www.fluttercampus.com/video.mp4':widget!.data!.link} ");
+    controller.addListener(() {
+      setState(() {});
+    });
+    controller.initialize().then((value){
+      setState(() {});
+    });
+
+  }
+  @override
+  void initState() {
+    loadVideoPlayer();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
-        setState(() {
-          controller.value.isPlaying
-              ? controller.pause()
-              : controller.play();
-        });
+      onTap: () {
+          if(controller.value.isPlaying){
+            controller.pause();
+          }else{
+            controller.play();
+          }
+
+          setState(() {
+
+          });
       },
-      child:Container(
-        height: 80,
-        width: 100,
-        child: controller.value.isInitialized
-            ? AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: VideoPlayer(controller),
-        )
-            : Container(),//video player controller
-        // allowScrubbing: true,
-
+      child:  AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: VideoPlayer(controller),
       ),
-
+        // allowScrubbing: true,
     );
   }
-
 }
+
 class SliderAboutWidget extends StatelessWidget {
   final Banners model;
 
@@ -222,8 +261,7 @@ class SliderAboutWidget extends StatelessWidget {
                         spreadRadius: 3)
                   ]),
               child: CachedNetworkImage(
-
-                imageUrl:'${APIData.domainLink}${model.thump}',
+                imageUrl: '${APIData.domainLink}${model.thump}',
                 fit: BoxFit.cover,
                 placeholder: (context, url) => Center(
                     heightFactor: 3,
